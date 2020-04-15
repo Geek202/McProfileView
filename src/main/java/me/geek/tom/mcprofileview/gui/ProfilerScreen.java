5e38a5f86de9e4b28a435a45b7d01326ca9b1011
Tom_The_Geek <tgbryan39@gmail.com>
2020-04-15 14:46:25 +0100

    Mostly works now. Could probably improve some things but I cba right now

--- src/main/java/me/geek/tom/mcprofileview/gui/ProfilerScreen.java
+++ src/main/java/me/geek/tom/mcprofileview/gui/ProfilerScreen.java
@@ -1,7 +1,9 @@
 package me.geek.tom.mcprofileview.gui;
 
+import com.google.common.collect.Lists;
 import me.geek.tom.mcprofileview.gui.widget.TreeBar;
 import me.geek.tom.mcprofileview.profile.ProfileFile;
+import me.geek.tom.mcprofileview.profile.tree.TreePart;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.FontRenderer;
 import net.minecraft.client.gui.screen.Screen;
@@ -11,6 +13,8 @@ import net.minecraft.util.text.StringTextComponent;
 import java.awt.*;
 import java.io.File;
 import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
 
 public class ProfilerScreen extends Screen {
 
@@ -19,7 +23,7 @@ public class ProfilerScreen extends Screen {
     private ProfileFile profile = null;
 
     private TreeBar bar = null;
-
+    private Button stepUp = null;
 
     public ProfilerScreen() {
         super(new StringTextComponent(""));
@@ -31,9 +35,9 @@ public class ProfilerScreen extends Screen {
     }
 
     @Override
-    public void render(int p_render_1_, int p_render_2_, float p_render_3_) {
+    public void render(int mouseX, int mouseY, float partialTicks) {
         this.renderBackground();
-        super.render(p_render_1_, p_render_2_, p_render_3_);
+        super.render(mouseX, mouseY, partialTicks);
         FontRenderer renderer = Minecraft.getInstance().fontRenderer;
         if (currentFile != null) {
             //int width = minecraft.getMainWindow().getScaledWidth();
@@ -42,12 +46,29 @@ public class ProfilerScreen extends Screen {
             int x = 100;
             int y = 16;
             renderer.drawStringWithShadow(text, x, y, 0xFFFFFFFF);
-            if (profile != null) {
+            if (profile != null && bar != null) {
                 renderer.drawStringWithShadow("Ran for " + profile.getTicks() + " ticks in " + profile.getTime()
                         + " ms. (Aprox " + profile.getAverageTps() + " tps)",
                         10, 35, 0xFFFFFFFF);
+
+                renderer.drawStringWithShadow(this.bar.currentParent.getName(), 10, 45, 0xFFFFFFFF);
+
+                if (bar.getMouseOver(mouseX, mouseY) != -1)
+                    this.drawTooltips(mouseX, mouseY);
             }
         }
+    }
+
+    private void drawTooltips(int mouseX, int mouseY) {
+        TreePart over = bar.currentParent.getChildren().get(bar.getMouseOver(mouseX, mouseY));
+
+        String[] pts = over.getName().split(" - ");
+        String[] percents = pts[1].split("/");
+        List<String> lines = new ArrayList<>();
+        lines.add(pts[0]);
+        lines.add(percents[0] + " of parent");
+        lines.add(percents[1] + "of total");
+        this.renderTooltip(lines, mouseX, mouseY);
     }
 
     private void openFile(Button button) {
@@ -69,9 +90,15 @@ public class ProfilerScreen extends Screen {
                 if (bar != null) {
                     this.buttons.remove(bar);
                     this.children.remove(bar);
+                } if (stepUp != null) {
+                    this.buttons.remove(stepUp);
+                    this.children.remove(stepUp);
                 }
 
-                bar = addButton(new TreeBar(10, 45, profile.getRoot()));
+                bar = addButton(new TreeBar(10, 65, profile.getRoot()));
+                stepUp = addButton(new Button(10, 90, 85, 20, "Step up", btn -> bar.updateDisplay(-1)));
+                stepUp.active = false;
+                bar.setCanStepUp(b -> stepUp.active = b);
             } catch (Exception e) {
                 e.printStackTrace();
             }
--- src/main/java/me/geek/tom/mcprofileview/gui/widget/TreeBar.java
+++ src/main/java/me/geek/tom/mcprofileview/gui/widget/TreeBar.java
@@ -2,18 +2,23 @@ package me.geek.tom.mcprofileview.gui.widget;
 
 import me.geek.tom.mcprofileview.profile.tree.TreePart;
 import me.geek.tom.mcprofileview.profile.tree.TreeRoot;
+import net.minecraft.client.MainWindow;
+import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.widget.Widget;
 
 import java.util.List;
+import java.util.function.Consumer;
 
 public class TreeBar extends Widget {
 
     private TreeRoot root;
-    private TreePart currentParent;
+    public TreePart currentParent;
+
+    private Consumer<Boolean> canStepUp;
 
     private List<TreePart> currentDisplay;
 
-    public static final int WIDTH = 800;
+    public static final int WIDTH = 400;
     public static final int HEIGHT = 20;
 
     private static final int[] COLOURS = new int[] {
@@ -34,35 +39,66 @@ public class TreeBar extends Widget {
         this.currentDisplay = this.currentParent.getChildren();
     }
 
+    public void setCanStepUp(Consumer<Boolean> canStepUp) {
+        this.canStepUp = canStepUp;
+    }
+
     @Override
     public void render(int p_render_1_, int p_render_2_, float p_render_3_) {
         int pos = 0;
+
+        fill(x, y, x + WIDTH, y + HEIGHT, 0xFF000000);
 
         for (int i = 0; i < this.currentDisplay.size(); i++) {
             if (this.currentDisplay.get(i).getParentPercent() == 0.0f)
                 continue;
             int col = i % COLOURS.length;
 
-            int width = (int) (8 * this.currentDisplay.get(i).getParentPercent());
-            fill(x + pos, y, x + width, y + HEIGHT, COLOURS[col]);
+            int width = (int) (4 * this.currentDisplay.get(i).getParentPercent());
+            fill(x + pos, y, x + pos + width, y + HEIGHT, COLOURS[col]);
             pos += width;
         }
     }
 
     @Override
     public void onClick(double x, double y) {
-        int idx = 0;
-        System.out.println(x + " / " + y);
+        int idx = getMouseOver(x, y);
 
-        for (int i = 0, pos = 0; i < this.currentDisplay.size(); i++) {
-            int w = (int)(8 * this.currentDisplay.get(i).getParentPercent());
-            if (x >= x + pos && x < pos + w) {
+        if (idx != -1)
+            updateDisplay(idx);
+    }
+
+    public int getMouseOver(double x, double y) {
+        if (!this.isMouseOver(x, y))
+            return -1;
+
+        int idx = -1;
+
+        for (int i = 0, pos = this.x; i < this.currentDisplay.size(); i++) {
+            int w = (int)(4 * this.currentDisplay.get(i).getParentPercent());
+            if (x >= pos && x < pos + w) {
                 idx = i;
                 break;
             }
             pos += w;
         }
 
-        System.out.println("Got section :" + idx + " clicked!");
+        return idx;
+    }
+
+    public void updateDisplay(int idx) {
+        if (idx >= 0) {
+            this.currentParent = this.currentDisplay.get(idx);
+            this.canStepUp.accept(true);
+        } else {
+            if (this.currentParent == this.root) {
+                System.out.println("Trying to step up from the root element! This should not happen, but has somehow!");
+                return;
+            }
+            this.currentParent = this.currentParent.getParent();
+            this.canStepUp.accept(this.currentParent != this.root);
+        }
+
+        this.currentDisplay = this.currentParent.getChildren();
     }
 }
